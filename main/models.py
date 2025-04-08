07d7a47369442cf51df1806ef085c59e844ac9d5
Lain0x15 <max0x15@outlook.com>
2025-04-08 23:46:20 +0300

    add Связанные

--- main/models.py
+++ main/models.py
@@ -14,6 +14,12 @@ class studios (models.Model):
     def __str__(self):
         return self.name
 
+class types_model (models.Model):
+    name = models.CharField(max_length=50)
+
+class franchises (models.Model):
+    name = models.CharField(max_length=50)
+
 class anime(models.Model):
     name = models.CharField(max_length=50)
     trailer = models.URLField(max_length=200)
@@ -22,9 +28,14 @@ class anime(models.Model):
     description = models.CharField(max_length=375)
     portraitImage = models.ImageField(upload_to='anime/portraitImage/')
     studio = models.ManyToManyField(studios)
-    
+    type_field = models.ForeignKey(types_model, on_delete=models.PROTECT, null=True)
+    franchise = models.ForeignKey(franchises, on_delete=models.PROTECT, null=False)
+
     def getGenres(self):
         return genreAnime.objects.filter(animeID=self)
+
+    def getFranchisesAnime(self):
+        return anime.objects.filter(franchise=self.franchise.id).order_by('releaseYear')
 
     def __str__(self):
         return self.name
