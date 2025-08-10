88c800354099106ba30b8e1da16f1f98a2831367
Lain0x15 <max0x15@outlook.com>
2025-08-10 14:28:20 +0300

    fx

--- extend_auth/views.py
+++ extend_auth/views.py
@@ -13,44 +13,38 @@ def profile(request):
 
 def signup(request):
     if request.method == 'GET':
-        form = signup_form()
-        return render(request, 'signup.html', { 'form': form})
+        return render(request, 'signup.html', {'form': signup_form()})
 
     if request.method == 'POST':
         form = signup_form(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            login(request, user)
-            return redirect('/')
-        else:
+        if not form.is_valid():
             return render(request, 'signup.html', {'form': form})
+        user = form.save(commit=False)
+        user.save()
+        login(request, user)
+        return redirect('/')
 
 @login_required
 def email_change(request):
     if request.method == 'GET':
-        form = email_change_form(request.user)
-        return render(request, "email_change.html", {'form':form})
+        return render(request, "email_change.html", {'form':email_change_form(request.user)})
 
     if request.method=='POST':
         form = email_change_form(request.user,request.POST)
-        if form.is_valid():
-            user = request.user
-            u = User.objects.get(username=user)
-            u.email = form.cleaned_data['new_email1']
-            u.save()
-            return redirect("/accounts/profile/")
-        else:
+        if not form.is_valid():
             return render(request, "email_change.html", {'form':form})
+        user = request.user
+        u = User.objects.get(username=user)
+        u.email = form.cleaned_data['new_email1']
+        u.save()
+        return redirect("/accounts/profile/")
+
 
 @login_required
 def avatar_change(request):
     if request.method=='POST':
         form = avatar_change_form(request.user, request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            print('yes')
-            return JsonResponse({'status': 'ok'})
-        else:
+        if not form.is_valid():
             return JsonResponse({'status': form.errors})
-        return JsonResponse({'status': 'wtf'})
+        form.save()
+        return JsonResponse({'status': 'ok'})
